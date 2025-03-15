7129da43ab69473061601633eeb7a38b109e6e44
98252 <166274297+98252@users.noreply.github.com>
2025-03-15 15:19:38 +0530

    Rahul TUT

---
+++ index.js
@@ -0,0 +1,114 @@
+//  console.log("Hello world");
+//  console.warn("this is warning");
+//  console.error("Given the error");
+//  var num1=56;
+//  var num2=45;
+//  console.log(num1+num2);
+//  var str2="this is rahul";
+//  var marks={
+//    rahul:45,
+//    kaxya:47,
+//     safal:88
+// }
+//  console.log(marks)
+//   var arr=[1,3,"rahul",4]
+//   var a=23
+//   var b=25
+//   console.log("The sum of a+b" ,a+b)
+//   console.log(a==b)
+//   console.log(a<b)
+//   console.log(a>b)
+//   function avg(a,b){
+//      return (a+b)/2
+
+//   }
+//   c1=(44,43)
+//   c2=(33,65)
+//   console.log(c1,c2)*
+//   var age=50
+//     if (age<55){
+//      console.log("you are young")
+//   }
+//   else{
+//     console.log("you are kid")
+//   }
+//  var arr=[1,2,3,4,5,6,7]
+//  arr.foreach(function(element){
+//      console.log(ele)
+//  })
+//  var arr=[1,2,3,4,5,6,7]
+//  let j=0;
+//  while (j<arr.length){
+//      console.log(arr[j])
+//      j++
+// }+
+//  var arr=[1,2,3,4,5,6,7]
+// let j=0;
+// do{
+//     console.log(j<arr[j])
+//     j++
+
+//  }while(j<arr.lenght)*/
+
+//  //let arr=["rahul",1,5,"safal",true,null]
+
+// //console.log(arr.length)
+// // arr.pop()
+// // console.log(arr)
+// // arr.push("Ragni")
+// // console.log(arr)
+// // arr.shift()
+// // console.log(arr)
+// // arr.unshift("pappu")
+// // console.log(arr)
+// //let myarray="Ragni is good girl "
+// ///console.log(myarray)
+// // d=myarray.replace("Ragni","Safal")
+// // console.log(d)
+// // mydate=new Date();
+// // console.log(mydate);
+// // console.log(mydate.getFullYear())
+// // dom manipulation
+// // let hello =document.getElementById('click')
+// // // console.log(hello)
+// //  let eleclass=document.getElementsByClassName('container')
+// //  console.log(eleclass)
+// // eleclass[0].ClassList.add("bgcolor")
+// // eleclass[0].ClassList.add("text sus")
+// // console.log(eleclass.innerText);
+// // tn=document.getElementsTagName('div')
+// // console.log(tn)
+// // createdElement=document.createElement('p')
+// // createdElement.innerText="Safal is chatgpt"
+// // tn[0].appendChild(createdElement)
+// // sel=document.querySelector('.container')
+// // console.log(sel)
+//  //function clicked(){
+// //      console.log("The button was clicked")
+// // }
+ 
+// // }
+// // firstcontainer.addEventListener('click' ,function(){
+// //     console.log('hogya')
+
+// // })
+// // firstcontainer.addEventListener('mouseover',function(){
+// //     console.log('mouse used')
+// // })
+// // firstcontainer.addEventListener('mouseout',function(){
+// //     console.log('mouse is out used')
+// // })
+// // firstcontainer.addEventListener('mouseup',function(){
+// //     console.log('mouse is up used')
+// // })
+// // function summ(a,b){
+// //     return a+b
+// // }
+// // lorkaro=()=>{
+// //     console.log('i am your log')
+// // }
+// // setTimeout(lorkaro,2000)
+// // //JSON
+// // obj={name:"Hari"}
+// // jso=JSON.stringify(obj)
+// // console.log(jso)
